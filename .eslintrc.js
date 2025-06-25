bf57ce0488f5c547bd8f77ff7ef6ff9ed0ae1b29
animeshxdev <vegan.athelete.animesh@gmail.com>
2025-06-25 13:11:54 +0530

    Fix ESLint/TypeScript for Vercel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
+// .eslintrc.js
 module.exports = {
-  extends: ["next"],
+  extends: ["next", "next/core-web-vitals"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
